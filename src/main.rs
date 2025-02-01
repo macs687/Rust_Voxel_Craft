fe9737f31814ab6db93987a904045d327fe4500a
macs365 <macs38.hest@mail.ru>
2025-02-01 15:17:30 +0700

    построение базовой структуры window and events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use window::{Window};
+
+mod window;
+
+
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
+const TITLE: &str = "Voxel_Craft";
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::init(WIDTH, HEIGHT, TITLE).unwrap();
+
+    unsafe {
+        gl::ClearColor(1.0, 1.0, 1.0, 1.0);
+        // gl::Enable(DEPTH_TEST);
+        // gl::Enable(gl::CULL_FACE);
+        // gl::Enable(gl::BLEND);
+        // gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+    }
+
+
+
+    while !window.should_close() {
+        window.poll_events();
+
+
+
+
+        window.swap_buffers();
+    }
+
+    window.terminate();
 }
---
+++ src/window/mod.rs
@@ -0,0 +1,64 @@
+mod events;
+
+extern crate gl;
+use gl::types::*;
+
+extern crate glfw;
+use glfw::{Glfw, PWindow, GlfwReceiver, WindowEvent, fail_on_errors, Action, Context, Key};
+use glfw::ffi::{glfwTerminate};
+
+pub struct Window{
+    pub glfw: Glfw,
+    pub window: PWindow,
+    pub receiver: GlfwReceiver<(f64, WindowEvent)>
+}
+
+
+impl Window {
+    pub fn init(width: u32, height: u32, title: &str) -> Result<Self, String> {
+        let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+        glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+        glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+        glfw.window_hint(glfw::WindowHint::Resizable(true));
+
+        let (mut window, events) = match glfw.create_window(width, height, title, glfw::WindowMode::Windowed) {
+            Some((window, events)) => (window, events),
+            None => {
+                eprintln!("Failed to create GLFW window.");
+                std::process::exit(1);
+            }
+        };
+
+        window.make_current();
+        gl::load_with(|s| window.get_proc_address(s) as *const _);
+        window.set_key_polling(true);
+
+        unsafe {
+            gl::Viewport(0, 0, width as i32, height as i32);
+        }
+
+        Ok(Self {glfw, window, receiver: events})
+    }
+
+    pub fn terminate(&mut self) {
+        unsafe { glfwTerminate() }
+    }
+
+    pub fn swap_buffers(&mut self){
+        self.window.swap_buffers();
+    }
+
+    pub fn should_close(&mut self) -> bool {
+        self.window.should_close()
+    }
+
+    pub fn close(&mut self){
+        self.window.set_should_close(true);
+    }
+
+    pub fn poll_events(&mut self){
+        self.glfw.poll_events();
+    }
+
+}
